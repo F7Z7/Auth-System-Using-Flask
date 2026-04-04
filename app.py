93dd270a3cff55497dd8dcc330cef00ecc753b20
Farzan R S <farzanrichu610@gmail.com>
2026-04-04 15:54:35 +0530

    temp setup

--- app.py
+++ app.py
@@ -9,5 +9,18 @@ app=Flask(__name__)
 def home():
     return render_template("login.html")
 
+@app.route("/login",methods=["POST"])
+def login():
+    role=request.form.get("role")
+    username=request.form.get("username")
+    password=request.form.get("password")
+
+    print(role, username, password)
+
+    if username=="admin" and password=="farza@2004":
+        return f"Welcome {role} {username}"
+
+    return "Invalid credentials", 401
+
 if __name__ == '__main__':
     app.run(debug=True)
