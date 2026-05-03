3ce9f248ed20b96f68aebcbfb86a1a4c659c33f0
Farzan R S <farzanrichu610@gmail.com>
2026-05-03 22:15:16 +0530

    first commit in what 1 months?? started building again

---
+++ auth.py
@@ -0,0 +1,13 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Auth:
+    @staticmethod
+    def hash_password(password):
+        return generate_password_hash(password)
+
+    @staticmethod
+    def verify_password(password, hashed_password):
+        return check_password_hash(hashed_password, password)
+
+
+
