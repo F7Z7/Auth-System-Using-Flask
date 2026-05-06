2ff00ed276d7302ddb84e5915ff19a8aaa08c9ba
Farzan R S <farzanrichu610@gmail.com>
2026-05-07 01:51:24 +0530

    render template for register.html

--- main.py
+++ main.py
@@ -8,23 +8,25 @@ app=Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/register",methods=["POST"])
+@app.route("/register",methods=["GET","POST"])
 def register():
-    role=request.form.get("selection")
-    username=request.form.get("username")
-    password=request.form.get("password")
+    if request.method=="POST":
+        role=request.form.get("selection")
+        username=request.form.get("username")
+        password=request.form.get("password")
 
-    hashed_password=generate_password_hash(password)
-    conn=get_db_connection()
+        hashed_password=generate_password_hash(password)
+        conn=get_db_connection()
 
-    conn.execute(
+        conn.execute(
         "INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
         (username, hashed_password, role)
-    )
+        )
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
 
-    return "User registered successfully"
+        return "User registered successfully"
+    return render_template("register.html")
 if __name__ == '__main__':
     app.run(debug=True)
