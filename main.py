3e346c5f5a4432a30cd23cc0549a40968c44faf5
Farzan R S <farzanrichu610@gmail.com>
2026-05-08 01:30:56 +0530

    logic for checking pass done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 
-from auth import generate_password_hash
+from auth import generate_password_hash,check_password_hash
 from db import get_db_connection
 
 app = Flask(__name__)
@@ -29,8 +29,36 @@ def register():
         conn.commit()
         conn.close()
 
-        return redirect(url_for("/login"))
+        return render_template("login.html")
     return render_template("register.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login_user():
+    role = request.form.get("selection")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    check_user(username,password)
+
+
+def check_user(username,password):
+    conn=get_db_connection()
+
+    result=conn.execute("SELECT * FROM users WHERE username = ?", (username,))
+
+
+    user=result.fetchone()
+    if user:
+        hash_pass=user["password"]
+
+        res=check_password_hash(hash_pass,password)
+
+        if res:
+            return render_template("welcome.html")
+
+    else:
+        return False
+
 
 
 if __name__ == '__main__':
